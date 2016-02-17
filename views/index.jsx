41156b5ac4b9236365a0d41f3e1e931cd29f82bc
Matt Sell <msell@onetechnologies.net>
2016-02-16 20:49:23 -0600

    done

--- views/index.jsx
+++ views/index.jsx
@@ -41,11 +41,11 @@ import React from 'react';
         render() {
             return (
                 <tr>
-                    <td style={{border: "1px solid black"}}>
+                    <td style={style.tableContent}>
                         <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
                     </td>
-                    <td style={{border: "1px solid black"}}>{this.props.title}</td>
-                    <td style={{border: "1px solid black"}}>{this.props.children}</td>
+                    <td style={style.tableContent}>{this.props.title}</td>
+                    <td style={style.tableContent}>{this.props.children}</td>
                 </tr>
             );
         }
@@ -63,3 +63,9 @@ import React from 'react';
         )
       }
     }
+    
+    let style = {
+        tableContent: {
+            border: "1px solid black"
+        }
+    };
