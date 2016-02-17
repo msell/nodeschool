d4218a743cffadb3af41fca93612b2c8b5385d57
Matt Sell <mwsell@gmail.com>
2016-02-16 19:54:46 -0600

    complete

--- views/index.jsx
+++ views/index.jsx
@@ -13,15 +13,31 @@ import React from 'react';
     }
 
     class TodoList extends React.Component {
+        render() {
+            return (
+                <div className="todoList">
+                    <table style={{border: "2px solid black"}}>
+                        <tbody>
+                        <Todo title="Shopping">Milk</Todo>
+                        <Todo title="Hair cut">13:00</Todo>
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
+    }
+
+    class Todo extends React.Component {
       render() {
         return (
-          <div className="todoList">
-            I am a TodoList.
-          </div>
+          <tr>
+            <td style={{border: "1px solid black"}}>{this.props.title}</td>
+            <td style={{border: "1px solid black"}}>{this.props.children}</td>
+           </tr>
         )
       }
     }
-
+    
     class TodoForm extends React.Component {
       render() {
         return (
